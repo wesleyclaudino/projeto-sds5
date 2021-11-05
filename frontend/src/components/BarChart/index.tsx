764bb06308dbc11f967406016e46cf32303f2565
Wesley Claudino <claudinowesley11@gmail.com>
2021-11-05 18:55:21 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,42 @@
+import axios from 'axios';
+import { round } from 'pages/utils/format';
+import { BASE_URL } from 'pages/utils/requests';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+
+type SeriesData = {
+    name: string
+    data: number[]
+}
+
+type ChartData = {
+    labels: {
+        categories: string[]
+    }
+    series: SeriesData[]
+}
 
 function BarChart() {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: { categories: [] },
+        series: [{ name: "", data: [] }]
+    })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSuccess[]
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1))
+
+                setChartData({
+                    labels: { categories: myLabels },
+                    series: [{ name: "% Sucesso", data: mySeries }]
+                })
+            })
+    }, [])
 
     const options = {
         plotOptions: {
@@ -10,23 +46,11 @@ function BarChart() {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-
     return (
         <div>
             <Chart 
-            options={{...options, xaxis: mockData.labels}}
-            series={mockData.series}
+            options={{...options, xaxis: chartData.labels}}
+            series={chartData.series}
             type="bar"
             height="240"
             />
